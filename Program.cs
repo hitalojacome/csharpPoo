5c04085021a9507e736c081fa8b4d6df38cd16c6
Hitalo Jacome <jacomehitalo@gmail.com>
2023-09-29 09:23:34 -0300

    Adicionando escopo do proneto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace workspace {
+    class Program {             
+        static void Main(string[] args) {
+
+            Console.Write("Entre com as medidas do triângulo X: ");
+            string[] vet = Console.ReadLine().Split(' ');
+            double aX = double.Parse(vet[0]);
+            double bX = double.Parse(vet[1]);
+            double cX = double.Parse(vet[2]);
+
+            Console.Write("Entre com as medidas do triângulo Y: ");
+            vet = Console.ReadLine().Split(' ');
+            double aY = double.Parse(vet[0]);
+            double bY = double.Parse(vet[1]);
+            double cY = double.Parse(vet[2]);
+
+            double p = (aX + bX + cX) / 2.0;     
+            double areaX = Math.Sqrt(p * (p - aX) * (p - bX) * (p - cX));
+
+            p = (aY + bY + cY) / 2.0;     
+            double areaY = Math.Sqrt(p * (p - aY) * (p - bY) * (p - cY));
+
+            Console.WriteLine($"Área de X = {areaX:F4}");
+            Console.WriteLine($"Área de Y = {areaY:F4}");
+
+            if (areaX > areaY) {
+                Console.WriteLine("Maior área = X");
+            } else {
+                Console.WriteLine("Maior área = Y");
+            }
+            
+        }
+    }
+}
---
+++ Triangulo.cs
@@ -0,0 +1,9 @@
+namespace workspace {
+    class Triangulo {
+
+        public double A; 
+        public double B; 
+        public double C;
+
+    }
+}
