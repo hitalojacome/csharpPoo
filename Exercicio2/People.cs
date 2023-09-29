aa65c58aa05af5bbdd2717ceb3ff39467c11c881
Hitalo Jacome <jacomehitalo@gmail.com>
2023-09-29 10:48:35 -0300

    class People

---
+++ Exercicio2/People.cs
@@ -0,0 +1,8 @@
+namespace Exercicio2 {
+    class People {
+
+        public string name;
+        public int age;
+        
+    }
+}
---
+++ Exercicio2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercicio2 {
+    class Program {
+        static void Main(string[] args) {
+
+            People a, b;
+
+            a = new People();
+            b = new People();
+            
+        }
+    }
+}
