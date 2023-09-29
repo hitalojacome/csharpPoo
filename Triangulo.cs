7e1a80ad985ad9fbf0ec88f991ac2f7146f47757
Hitalo Jacome <jacomehitalo@gmail.com>
2023-09-29 09:25:47 -0300

    Classe Triangulo

--- Triangulo.cs
+++ Triangulo.cs
@@ -1,9 +1,10 @@
 namespace workspace {
     class Triangulo {
 
+        // Classe básica para solução do problema
         public double A; 
         public double B; 
-        public double C;
+        public double C; 
 
     }
 }
