5957a8f58810dd28991a0abf71b50cb88efba0e7
Hitalo Jacome <jacomehitalo@gmail.com>
2023-10-02 10:36:43 -0300

    Calculo de um retangulo

--- Exercicio5/Program.cs
+++ Exercicio5/Program.cs
@@ -4,7 +4,17 @@ namespace Exercicio5 {
     class Program {
         static void Main(string[] args) {
 
-            
+            Retangle x;
+            x = new Retangle();
+
+            Console.Write("Entre com a largura e altura do retângulo: ");
+            string[] values = Console.ReadLine().Split(' ');
+            x.Width = double.Parse(values[0]);
+            x.Height = double.Parse(values[1]);
+
+            Console.WriteLine($"AREA = {x.Area():F2}");
+            Console.WriteLine($"PERÍMETRO = {x.Perimeter():F2}");
+            Console.WriteLine($"DIAGONAL = {x.Diagonal():F2}");
             
         }
     }
--- Exercicio5/Rectangle.cs
+++ Exercicio5/Rectangle.cs
@@ -4,6 +4,19 @@ namespace Exercicio5 {
         public double Width;
         public double Height;
 
-        
+        // Calculo da area do retangulo
+        public double Area() {
+            return Width * Height;
+        }
+
+        // Calculo do perimetro do retangulo
+        public double Perimeter() {
+            return 2 * (Width + Height); 
+        }
+
+        // Calculo da diagonal do retangulo
+        public double Diagonal() {
+            return Math.Sqrt(Math.Pow(Width, 2) + Math.Pow(Height, 2));
+        }
     }
 }
