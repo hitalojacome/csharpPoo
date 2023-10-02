3015faa1cce0eaa918ed514ef61fe03bda490d3f
Hitalo Jacome <jacomehitalo@gmail.com>
2023-10-02 10:44:08 -0300

    Exercicio 6

---
+++ Exercicio6/Employee.cs
@@ -0,0 +1,5 @@
+namespace Exercicio6 {
+    class Employee {
+        
+    }
+}
---
+++ Exercicio6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicio6 {
+    class Program {
+        static void Main(string[] args) {
+
+            Employee x;
+            x = new Employee();
+            
+        }
+    }
+}
