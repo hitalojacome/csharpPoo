9ccb7d17d09bdf9b392bb2d8c0d2b08d21ea771e
Hitalo Jacome <jacomehitalo@gmail.com>
2023-10-02 10:59:57 -0300

    Promoção Salárial

--- Exercicio6/Employee.cs
+++ Exercicio6/Employee.cs
@@ -1,5 +1,21 @@
 namespace Exercicio6 {
     class Employee {
+
+        public string Name;
+        public double GrossSalary;
+        public double Tax;
+
+        public double NetSalary() {
+            return GrossSalary - Tax;
+        }
+
+        public void IncreaseSalary(double percentage) {
+            GrossSalary += GrossSalary * (percentage / 100.00);
+        }
+
+        public override string ToString() {
+            return ($"{Name}, $ {NetSalary():F2}.");
+        }
         
     }
 }
--- Exercicio6/Program.cs
+++ Exercicio6/Program.cs
@@ -6,7 +6,24 @@ namespace Exercicio6 {
 
             Employee x;
             x = new Employee();
-            
+
+            Console.Write("Nome: ");
+            x.Name = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            x.GrossSalary = double.Parse(Console.ReadLine());
+            Console.Write("Impostos: ");
+            x.Tax = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine($"Funcionário: {x}");
+            Console.WriteLine();
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double percentage = double.Parse(Console.ReadLine());
+            x.IncreaseSalary(percentage);
+            Console.WriteLine();
+
+            Console.WriteLine($"Dados atualizados: {x}");
         }
     }
 }
