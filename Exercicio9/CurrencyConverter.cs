acccbb69da37da95cf70ab18c93b7bba399e9ed0
Hitalo Jacome <jacomehitalo@gmail.com>
2023-10-03 11:14:24 -0300

    Exercicio 9

---
+++ Exercicio9/CurrencyConverter.cs
@@ -0,0 +1,5 @@
+namespace Exercicio9 {
+    class CurrencyConverter {
+        
+    }
+}
---
+++ Exercicio9/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Exercicio9 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Qual é a cotação do dólar");
+            
+        }
+    }
+}
