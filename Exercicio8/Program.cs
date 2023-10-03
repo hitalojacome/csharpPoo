bbc0be064073851b2e817d500eb40247bea946e6
Hitalo Jacome <jacomehitalo@gmail.com>
2023-10-03 10:03:26 -0300

    Membros estáticos 1

--- Exercicio8/Program.cs
+++ Exercicio8/Program.cs
@@ -9,14 +9,21 @@ namespace Exercicio8 {
             return 2 * Pi * raio;
         }
 
+        static double Volume(double raio) {
+            return 4.0 / 3.0 * Pi * Math.Pow(raio,3);
+        }
+
         static void Main(string[] args) {
 
             Console.Write("Entre o valor do raio: ");
             double raio = double.Parse(Console.ReadLine());
 
-            double circ = Circunferencia (raio);
+            double circ = Circunferencia(raio);
+            double volume = Volume(raio);
 
             Console.WriteLine($"Circunferência: {circ:F2}");
+            Console.WriteLine($"Volume: {volume:F2}");
+            Console.WriteLine($"Valor de Pi: {Pi:F2}");
             
         }
     }
