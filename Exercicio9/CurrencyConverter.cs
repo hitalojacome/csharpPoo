e426e9969a1631dc5728bd9a101b4298927a3901
Hitalo Jacome <jacomehitalo@gmail.com>
2023-10-03 11:18:29 -0300

    Cotação de moedas

--- Exercicio9/CurrencyConverter.cs
+++ Exercicio9/CurrencyConverter.cs
@@ -1,5 +1,11 @@
 namespace Exercicio9 {
     class CurrencyConverter {
-        
+
+        public static double Iof = 6.0;
+
+        public static double DolarToReal(double quantity, double priceQuote) {
+            double total = quantity * priceQuote;
+            return total + total * Iof / 100.0;
+        }
     }
 }
--- Exercicio9/Program.cs
+++ Exercicio9/Program.cs
@@ -4,7 +4,15 @@ namespace Exercicio9 {
     class Program {
         static void Main(string[] args) {
 
-            Console.Write("Qual é a cotação do dólar");
+            Console.Write("Qual é a cotação do dólar? ");
+            double priceQuote = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantos dólares você irá comprar? ");
+            double quantity = double.Parse(Console.ReadLine());
+
+            double result = CurrencyConverter.DolarToReal(quantity, priceQuote);
+
+            Console.WriteLine($"Valor a ser pago em reais = R$ {result:F2}");
             
         }
     }
