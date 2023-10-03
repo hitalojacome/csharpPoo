c3b8eb8c1e2f0902a35404c33f6ca5c6862330e9
Hitalo Jacome <jacomehitalo@gmail.com>
2023-10-03 11:11:24 -0300

    Métodos estáticos

---
+++ Exercicio8/Calculadora.cs
@@ -0,0 +1,24 @@
+namespace Exercicio8 {
+    class Calculadora {
+        /* Segunda solução
+        public double Pi = 3.14;
+
+        public double Circunferencia(double raio) {
+            return 2 * Pi * raio;
+        }
+
+        public double Volume(double raio) {
+            return 4.0 / 3.0 * Pi * Math.Pow(raio,3);
+        }
+        */
+        public static double Pi = 3.14;
+
+        public static double Circunferencia(double raio) {
+            return 2 * Pi * raio;
+        }
+
+        public static double Volume(double raio) {
+            return 4.0 / 3.0 * Pi * Math.Pow(raio,3);
+        }
+    }
+}   
--- Exercicio8/Program.cs
+++ Exercicio8/Program.cs
@@ -2,8 +2,8 @@
 
 namespace Exercicio8 {
     class Program {
-
-        static double Pi = 3.14;
+        /* Está primeira forma estaremos resolvendo com membros estáticos
+        static double Pi = 3.14; // Variável estática 
 
         static double Circunferencia(double raio) {
             return 2 * Pi * raio;
@@ -24,6 +24,35 @@ namespace Exercicio8 {
             Console.WriteLine($"Circunferência: {circ:F2}");
             Console.WriteLine($"Volume: {volume:F2}");
             Console.WriteLine($"Valor de Pi: {Pi:F2}");
+        }
+        // Agora resolveremos com uma classe instanciada
+        static void Main(string[] args) {
+
+            Calculadora calc = new Calculadora(); // Para chamar uma classe precisamos criar um objeto
+
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine());
+
+            double circ = calc.Circunferencia(raio);
+            double volume = calc.Volume(raio);
+
+            Console.WriteLine($"Circunferência: {circ:F2}");
+            Console.WriteLine($"Volume: {volume:F2}");
+            Console.WriteLine($"Valor de Pi: {calc.Pi:F2}");
+            
+        }*/
+
+        static void Main(string[] args) {
+
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine());
+
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine($"Circunferência: {circ:F2}");
+            Console.WriteLine($"Volume: {volume:F2}");
+            Console.WriteLine($"Valor de Pi: {Calculadora.Pi:F2}");
             
         }
     }
