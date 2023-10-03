068d14052718ac2f5baf813cf14810d206aa0396
Hitalo Jacome <jacomehitalo@gmail.com>
2023-10-03 09:58:44 -0300

    Exercicio 8

---
+++ Exercicio8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio8 {
+    class Program {
+
+        static double Pi = 3.14;
+
+        static double Circunferencia(double raio) {
+            return 2 * Pi * raio;
+        }
+
+        static void Main(string[] args) {
+
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine());
+
+            double circ = Circunferencia (raio);
+
+            Console.WriteLine($"Circunferência: {circ:F2}");
+            
+        }
+    }
+}
