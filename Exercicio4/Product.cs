5354f6a098d32c98f30a6ec26dcc30320995b549
Hitalo Jacome <jacomehitalo@gmail.com>
2023-10-06 10:34:41 -0300

    Alterando atributos publicos para privados

--- Exercicio4/Product.cs
+++ Exercicio4/Product.cs
@@ -1,34 +1,34 @@
 namespace Exercicio4 {
     class Product {
         // Atributos
-        public string Name;
-        public double Price;
-        public int Quantity;
+        private string _name;
+        private double _price;
+        private int _quantity;
 
         // Construtor
         public Product(string name, double price, int quantity) {
-            Name = name; Price = price; Quantity = quantity;
+            _name = name; _price = price; _quantity = quantity;
         }
 
         // Métodos
         // Método ToString
         public override string ToString() {
-            return ($"{Name}, $ {Price:F2}, {Quantity} unidades, Total: $ {TotalValueInStock():F2}.");
+            return ($"{_name}, $ {_price:F2}, {_quantity} unidades, Total: $ {TotalValueInStock():F2}.");
         }
 
         // Método para calcular o valor total em estoque
         public double TotalValueInStock() {
-            return Quantity * Price;
+            return _quantity * _price;
         }
 
         // Adiciona produtos
         public void AddProduct(int quantity) {
-            Quantity += quantity;
+            _quantity += quantity;
         }
 
         // Remove produtos
         public void RemoveProduct(int quantity) {
-            Quantity -= quantity;
+            _quantity -= quantity;
         }
 
 
