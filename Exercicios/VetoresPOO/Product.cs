c296c746f87ce13e6644ee9a02242122855c25ab
Hitalo Jacome <jacomehitalo@gmail.com>
2023-10-10 09:00:43 -0300

    Vetores com POO

---
+++ Exercicios/VetoresPOO/Product.cs
@@ -0,0 +1,7 @@
+namespace VetPoo {
+    class Product {
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Exercicios/VetoresPOO/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace VetPoo {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Insira quantos produtos")
+            
+        }
+    }
+}
