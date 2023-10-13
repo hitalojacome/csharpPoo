82a179cd3ffa2e9a5784de9eb4f9e865bb818747
Hitalo Jacome <jacomehitalo@gmail.com>
2023-10-13 10:49:14 -0300

    Exercicio de listas

---
+++ Listas/ListPOO01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ListPoo {
+    class Program {
+        static void Main(string[] args) {
+
+            List<string> nomes = new List<string> {"Maria", "José", "Claudio", "mateus", "Mauricio"};
+
+            nomes.RemoveAll(x => x[0] == 'M');
+
+            int i=0;
+            foreach (string nome in nomes) {
+                Console.WriteLine($"Usuário {i}: {nome}");
+                i++;
+            }
+            
+        }
+    }
+}
