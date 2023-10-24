f8591a56d5f8ad7fa095b8c8c57e8b9b8a1db6d0
Hitalo Jacome <jacomehitalo@gmail.com>
2023-10-24 13:20:25 -0300

    Exercício 2 de enum

--- Enum/Exercício02/Entities/Post.cs
+++ Enum/Exercício02/Entities/Post.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace Exercício02.Entities
 {
     public class Post
@@ -11,6 +13,30 @@ namespace Exercício02.Entities
         public Post () {}
         public Post (DateTime moment, string title, string content, int likes) {
             Moment = moment; Title = title; Content = content; Likes = likes;
+        }
+
+        public void AddComment(Comment comment) {
+            Comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment) {
+            Comments.Remove(comment);
+        }
+
+        public override string ToString() {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments:");
+
+            foreach (Comment c in Comments) {
+                sb.AppendLine(c.Text);
+            }
+
+            return sb.ToString();
         }
     }
 }
