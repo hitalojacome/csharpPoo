4c87feabf3db04e745f13ec5fad58a6b803ce0a5
Hitalo Jacome <jacomehitalo@gmail.com>
2023-10-27 08:31:22 -0300

    classe derivada SavingAccount

--- Herança-aula/Entities/SavingAccount.cs
+++ Herança-aula/Entities/SavingAccount.cs
@@ -1,7 +1,17 @@
 namespace Herança_aula.Entities
 {
-    public class SavingAccount
+    public class SavingAccount : Account
     {
-        
+        public double InterestRate { get; set; }
+
+        public SavingAccount () {}
+
+        public SavingAccount (int number, string holder, double balance, double interestRate) : base (number, holder, balance) {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance () {
+            Balance += Balance * InterestRate;
+        }
     }
 }
