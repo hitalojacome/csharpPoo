387bd4b721355fcd96e8848e512415498efca4b5
Hitalo Jacome <jacomehitalo@gmail.com>
2023-11-01 11:08:12 -0300

    Criando exceção

---
+++ tryCatch01/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,7 @@
+namespace tryCatch01.Entities.Exceptions
+{
+    public class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message) {}
+    }
+}
