3067f8729d207c8147985ca57d1e5a6de90fc5f1
Hitalo Jacome <jacomehitalo@gmail.com>
2023-11-29 13:30:08 -0300

    Files

---
+++ Files/Entities/Product.cs
@@ -0,0 +1,19 @@
+namespace Files.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity) {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double Total() {
+            return Price * Quantity;
+        }
+    }
+}
